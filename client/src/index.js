9d26bcfbc8733684eaff75c2ed3aff8b9d29a586
Chidimma <chidimmamofodum@gmail.com>
2023-12-04 10:27:41 -0500

    bootstrap added

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.js";
 
 createRoot(document.getElementById("root")).render(
