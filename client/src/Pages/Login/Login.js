903bbccb7c30cb5bdd335bef3aad9a6c6087ed24
Johng117 <83654331+Johng117@users.noreply.github.com>
2023-12-06 16:37:58 +0000

    Update Login.js

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -41,51 +41,53 @@ const Login = () => {
 	};
 
 	return (
-		<div className="container mt-5 p-4 text-center">
-			<h3> Deskeando Login</h3>
-			<form
-				onSubmit={handleSubmit}
-				className="form-control-lg"
-				// noValidate="novalidate"
-			>
-				<div className="row p-1">
-					<div className="col">
-						<label className="email-label">Email:</label>
+		<div className="container-sm mt-5 p-4 text-center">
+			<div className="col">
+				<h3> Deskeando Login</h3>
+				<form
+					onSubmit={handleSubmit}
+					className="form-control-sm"
+					// noValidate="novalidate"
+				>
+					<div className="row p-1 justify-content-center">
+						<div className="col-2 fs-xs-6 fs-sm-4 p-0">
+							<label className="email-label">Email:</label>
+						</div>
+						<div className="col-5 col-sm-4 p-0 text-start" >
+							<input
+								type="email"
+								name="email"
+								value={email.toLowerCase()}
+								onChange={(e) => handleInputs(e)}
+							/>
+						</div>
 					</div>
-					<div className="col">
-						<input
-							type="email"
-							name="email"
-							value={email.toLowerCase()}
-							onChange={(e) => handleInputs(e)}
-						/>
-					</div>
-				</div>
 
-				<div className="row p-1">
-					<div className="col">
-						<label className="password-label">Password:</label>
+					<div className="row p-1 justify-content-center">
+						<div className="col-3 col-sm-2 p-0 ">
+							<label className="password-label">Password:</label>
+						</div>
+						<div className="col-5 col-sm-4 p-0 text-start">
+							<input
+								type="password"
+								name="password"
+								value={password}
+								onChange={(e) => handleInputs(e)}
+								required
+							/>
+						</div>
 					</div>
-					<div className="col">
-						<input
-							type="password"
-							name="password"
-							value={password}
-							onChange={(e) => handleInputs(e)}
-							required
-						/>
+					{emailError && <h6>email error</h6>}
+					{passwordError && <h6>password error</h6>}
+					<div className=" row pt-2">
+						<div className="col">
+							<button type="submit" className="btn btn-primary">
+								Login
+							</button>
+						</div>
 					</div>
-				</div>
-				{emailError && <h6>email error</h6>}
-				{passwordError && <h6>password error</h6>}
-				<div className=" row pt-2">
-					<div className="col">
-						<button type="submit" className="btn btn-primary">
-							Login
-						</button>
-					</div>
-				</div>
-			</form>
+				</form>
+			</div>
 		</div>
 	);
 };
