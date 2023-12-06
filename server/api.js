b792c91cb0b998b032b8276eba6fcaca2c0391fa
JanefrancessC <janefrancesschioma@gmail.com>
2023-12-06 14:20:07 +0000

    check is_admin

--- server/api.js
+++ server/api.js
@@ -1,52 +1,67 @@
 import { Router } from "express";
 import db from "./db.js";
 import logger from "./utils/logger.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
 const router = Router();
 
 router.get("/", (_, res) => {
-  logger.debug("Welcoming everyone...");
-  res.json({ message: "Hello from Deskeando!" });
+	logger.debug("Welcoming everyone...");
+	res.json({ message: "Hello from Deskeando!" });
 });
 
-
 router.post("/signup", async (req, res) => {
-  try {
-    const { first_name, last_name, role, email, password } = req.body;
+	try {
+		const { first_name, last_name, department, email, password } =
+			req.body;
 
-    const user = await db.query(`SELECT * FROM users WHERE email = $1`, [email])
+		const user = await db.query(`SELECT * FROM users WHERE email = $1`, [
+			email,
+		]);
 
-    if (user.rows.length > 0) {
-      res.status(401).json("User already exists")
-    }
-    const hash = await bcrypt.hash(JSON.stringify(password), 10)
+		if (user.rows.length > 0) {
+			res.status(401).json("User already exists");
+		}
 
-    const newUser = await db.query(`INSERT INTO users (first_name, last_name, role, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING *`, [first_name, last_name, role, email, hash])
-    res.status(201).json({ message: "User successfully created" })
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ error: error.message })
-  }
-})
+    let is_admin = false;
+		if (department.toUpperCase() === "HR") {
+		  is_admin = true;
+		}
+
+		const passwordString = String(password);
+		const hash = await bcrypt.hash(passwordString, 10);
+		
+		const newUser = await db.query(
+			`INSERT INTO users (first_name, last_name, department, is_admin, email, passwd)
+		  VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+			[first_name, last_name, department, is_admin, email, hash]
+		);
+		res.status(201).json({ message: "User successfully created" });
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).json({ error: "Server error" });
+	}
+});
 
 router.post("/login", async (req, res) => {
-  try {
-    const {  email, password } = req.body
-    const user = await db.query(`SELECT * FROM users WHERE email = $1`, [email])
+	try {
+		const { email, password } = req.body;
+		const user = await db.query(`SELECT * FROM users WHERE email = $1`, [
+			email,
+		]);
 
-    if (!user) {
-      res.status(400).json({ error: "User not found"})
-      return
-    }
+		if (!user) {
+			res.status(400).json({ error: "User not found" });
+			return;
+		}
 
-    if (email === user.rows[0].email) {
-      res.json(user.rows[0])
-    }
-  } catch (error) {
-    console.error(error.message)
-    res.status(400).json({ error: error.message })
-  }
-})
+		if (email === user.rows[0].email) {
+			res.json(user.rows[0]);
+		}
+	} catch (error) {
+		console.error(error.message);
+		res.status(400).json({ error: "Server error" });
+	}
+});
 
 export default router;
