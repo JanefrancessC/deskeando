eaf6e342ef1c93ea1513fe9d8fa1137c54eab86e
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-12-07 19:57:58 +0000

    signup-page

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./Pages/About.js";
 import Home from "./Pages/Home.js";
+import Signup from "./Pages/Signup/Signup.js";
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/about/this/site" element={<About />} />
+    <Route path="/signup" element={<Signup />} />
   </Routes>
 );
 
