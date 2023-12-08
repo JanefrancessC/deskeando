89a4e6c693dd6ee259e605f78eaef8609db562b8
Johng117 <83654331+Johng117@users.noreply.github.com>
2023-12-08 14:55:15 +0000

    Updates
    Updates to Login.css and Login.js.

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -4,42 +4,42 @@ import "./Home.css";
 import logo from "./logo.svg";
 
 export function Home() {
-  const [message, setMessage] = useState("Loading...");
+	const [message, setMessage] = useState("Loading...");
 
-  useEffect(() => {
-    fetch("/api")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(res.statusText);
-        }
-        return res.json();
-      })
-      .then((body) => {
-        setMessage(body.message);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+	useEffect(() => {
+		fetch("/api")
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error(res.statusText);
+				}
+				return res.json();
+			})
+			.then((body) => {
+				setMessage(body.message);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
 
-  return (
-    <main role="main">
-      <div>
-        <img
-          className="logo"
-          data-qa="logo"
-          src={logo}
-          alt="Just the React logo"
-        />
-        <h1 className="message" data-qa="message">
-          {message}
-        </h1>
-        <Link to="/about/this/site">About</Link>
-        <br></br>
-        <Link to="/login/here">Login</Link>
-      </div>
-    </main>
-  );
+	return (
+		<main role="main">
+			<div>
+				<img
+					className="logo"
+					data-qa="logo"
+					src={logo}
+					alt="Just the React logo"
+				/>
+				<h1 className="message" data-qa="message">
+					{message}
+				</h1>
+				<Link to="/about/this/site">About</Link>
+				<br></br>
+				<Link to="/login">Login</Link>
+			</div>
+		</main>
+	);
 }
 
 export default Home;
--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -9,22 +9,23 @@ const Login = () => {
 	const navigate = useNavigate();
 
 	// state for login credentials
-	const [inputs, setInputs] = useState({
+	const [formData, setFormData] = useState({
 		email: "",
 		password: "",
 	});
 
 	// state for login errors
 	const [loginError, setLoginError] = useState(false);
+	const [isValid, setIsValid] = useState(true);
 
-	const { email, password } = inputs;
+	const { email, password } = formData;
 
 	// function to handle inputted email and password
-	const handleInputs = (e) => {
-		setInputs({ ...inputs, [e.target.name]: e.target.value });
+	const handleFormData = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
 
-	// const checkInputs = (checkEmail, checkPassword) => {
+	// const checkFormData = (checkEmail, checkPassword) => {
 	// 	let isEmail = /[a-zA-Z0-9]{3}@[a-zA-Z0-9\.]{4}/g.test(checkEmail);
 	// 	let isPassword = checkPassword.length >= 3;
 	// 	isEmail === false ? setEmailError(true) : null;
@@ -46,7 +47,7 @@ const Login = () => {
 					navigate("/");
 				} else {
 					setLoginError(true);
-				    setInputs({ email: "", password: "" });
+					setFormData({ email: "", password: "" });
 				}
 			})
 			.catch((error) => {
@@ -57,60 +58,97 @@ const Login = () => {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+		// if (formData.email.trim() === "" || formData.password.trim() === "") {
+		// 	setIsValid(false);
+		// 	return;
+		// }
 		setLoginError(false);
 		tryLogin();
 	};
 
 	return (
-		<div className="container mt-5">
-			<div className="row justify-content-center ">
-				<h3 className="title-text text-center p-0">Deskeando</h3>
-			</div>
-			<div className="row justify-content-center">
-				<div className="col-7 col-sm-7 col-md-6 col-lg-5 col-xl-4 align-self-center">
-					<div className="row">
-						<h3 className=" col-2 p-0 sign-in text-center">Sign in</h3>
-					</div>
-					<form onSubmit={handleSubmit} className="mt-4 px-2">
-						<div className="row">
-							<input
-								type="email"
-								name="email"
-								placeholder="Email"
-								value={inputs.email.toLowerCase()}
-								onChange={(e) => handleInputs(e)}
-								required
-							/>
-						</div>
-						<div className="row mt-4">
-							<input
-								type="password"
-								name="password"
-								placeholder="Password"
-								value={inputs.password}
-								onChange={(e) => handleInputs(e)}
-								required
-							/>
-						</div>
-
-						<div className=" row pt-2 mt-4">
-							<div className="col">
-								<button
-									type="submit"
-									className="btn btn-primary btn-block w-100"
+		<section className="vh-100">
+			<div className="container py-5 h-75">
+				<div className="row d-flex justify-content-center align-items-center">
+					<div className="col-12 col-md-8 col-lg-6 col-xl-5">
+						<div className="card shadow-2-strong ">
+							<div className="card-body p-5 text-center">
+								<p className="text-center h1 sign_up-heading fw-bold p-2">
+									Deskeando
+								</p>
+								<h3 className="form-group d-flex flex-row p-1">Sign in</h3>
+								&nbsp;
+								<form
+									onSubmit={handleSubmit}
+									className={`w-100 requires-validation ${
+										!isValid && "was - validated"
+									}`}
+									novalidate
 								>
-									Login
-								</button>
+									<div className="form-group">
+										<div className="d-flex flex-row p-2">
+											<input
+												className="form-control w-100 form-control-lg"
+												type="email"
+												name="email"
+												id="email"
+												placeholder="Enter Email"
+												value={formData.email.toLowerCase()}
+												onChange={(e) => handleFormData(e)}
+												maxLength="500"
+												required
+											/>
+											<div
+												id="validationEmail"
+												className="invalid-feedback text-start"
+											>
+												Please enter your email address.
+											</div>
+										</div>
+									</div>
+									<div className="form-group">
+										<div className="d-flex flex-row p-2">
+											<input
+												className="form-control w-100 form-control-lg"
+												type="password"
+												name="password"
+												placeholder="Enter Password"
+												value={formData.password}
+												onChange={(e) => handleFormData(e)}
+												maxLength="200"
+												required
+											/>
+											<div
+												id="validationPassword"
+												className="invalid-feedback text-start"
+											>
+												Please enter your password.
+											</div>
+										</div>
+									</div>
+									<br />
+									<div className="row d-flex justify-content-center p-3">
+										<button
+											type="submit"
+											className="btn sign_in_btn btn-lg w-100"
+										>
+											Sign In
+										</button>
+									</div>
+									{loginError && (
+										<h6>
+											Login error: Please enter the correct email and password.
+										</h6>
+									)}
+								</form>
 							</div>
 						</div>
-						{loginError && (
-							<h6>Login error: Please enter the correct email and password.</h6>
-						)}
-					</form>
+					</div>
 				</div>
 			</div>
-		</div>
+		</section>
 	);
 };
 
 export default Login;
+
