39af41b26ec5b7e0e6b99f89ff4040799eaa6395
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-12-09 10:38:05 +0000

    fixing errors

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import About from "./Pages/About.js";
 import Dashboard from "./Pages/AdminDash/Dashboard.js";
 import Home from "./Pages/Home.js";
-<<<<<<< HEAD
-import Signup from "./Pages/Signup/Signup.js";
-
-const App = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about/this/site" element={<About />} />
-    <Route path="/signup" element={<Signup />} />
-  </Routes>
-=======
 import EmployeeDsh from "./Pages/EmployeeDash/EmployeeDsh.js";
 import SignIn from "./Pages/SignIn/SignIn.js";
+import Signup from "./Pages/Signup/Signup";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/signin" element={<SignIn />} />
+		<Route path="/signup" element={<Signup />} />
 		<Route path="/about/this/site" element={<About />} />
 		<Route path="/admin" element={<Dashboard />} />
-		<Route path="/employee" element={<EmployeeDsh/>} />
+		<Route path="/employee" element={<EmployeeDsh />} />
 	</Routes>
->>>>>>> 9678349c9cc097ed6d7aa1a96d00d91617970d6e
 );
 
 export default App;
