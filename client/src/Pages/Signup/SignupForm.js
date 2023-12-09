57d0a5a05107c8d104b2ff4de3fd3788bf2db74d
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-12-09 10:01:07 +0000

    completed design Sign Up page

--- client/src/Pages/Signup/SignupForm.js
+++ client/src/Pages/Signup/SignupForm.js
@@ -1,39 +1,78 @@
 import React, { useState, useEffect } from "react";
 import "./signupForm.css";
+import { Link } from "react-router-dom";
 
 function SignUp() {
 	const [isValid, setIsValid] = useState(true);
 	const [formData, setFormData] = useState({
 		firstName: "",
 		lastName: "",
-		department: "", // Added department state
+		department: "",
+		email: "",
+		password: "",
+	});
+	const [validationErrors, setValidationErrors] = useState({
 		email: "",
 		password: "",
 	});
 	const [signUpError, setSignUpError] = useState(false);
 
 	const handleInputChange = (event) => {
-		let { name, value } = event.target;
+		const { name, value } = event.target;
 		setFormData({ ...formData, [name]: value });
+
+		// Validate email on input change
+		if (name === "email") {
+			setValidationErrors((prevErrors) => ({
+				...prevErrors,
+				email: validateEmail(value)
+					? ""
+					: "Please enter a valid email address.",
+			}));
+		}
+
+		if (name === "password") {
+			setValidationErrors((prevErrors) => ({
+				...prevErrors,
+				password:
+					value.trim().length >= 6
+						? ""
+						: "Password must have at least 6 characters.",
+			}));
+		}
+	};
+
+	const validateEmail = (email) => {
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		return emailRegex.test(email);
 	};
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		if (
-			formData.email.trim() === "" ||
-			formData.password.trim() === "" ||
-			formData.firstName.trim() === "" ||
-			formData.lastName.trim() === "" ||
-			formData.department.trim() === ""
-		) {
+
+		if (!validateEmail(formData.email)) {
+			setValidationErrors((prevErrors) => ({
+				...prevErrors,
+				email: "Please enter a valid email address.",
+			}));
 			setIsValid(false);
 			setSignUpError(true);
 			return;
 		}
-		// Your other logic for form submission
 
-		// If submission is successful, you can reset the form and error state
+		// Validate password
+		if (formData.password.trim().length < 6) {
+			setValidationErrors((prevErrors) => ({
+				...prevErrors,
+				password: "Password must have at least 6 characters.",
+			}));
+			setIsValid(false);
+			setSignUpError(true);
+			return;
+		}
+
 		resetForm();
+		setIsValid(true);
 		setSignUpError(false);
 	};
 
@@ -45,11 +84,13 @@ function SignUp() {
 			email: "",
 			password: "",
 		});
+		setValidationErrors({
+			email: "",
+			password: "",
+		});
 	};
 
-	useEffect(() => {
-		// Your useEffect logic, e.g., toast("There was an error loading this page.");
-	}, []);
+	useEffect(() => {}, []);
 
 	return (
 		<section className="vh-100">
@@ -58,13 +99,16 @@ function SignUp() {
 					<div className="col-12 col-md-8 col-lg-6 col-xl-5">
 						<div className="card shadow-2-strong">
 							<div className="card-body p-5 text-center">
-								<p className="text-center h1 sign_up-heading fw-bold p-2">
+								<p
+									id="title"
+									className="text-center h1 sign_up-heading fw-bold p-2"
+								>
 									Deskeando
 								</p>
-								<h3 className="form-group d-flex flex-row p-1">Sign in</h3>
+								<h3 className="form-group d-flex flex-row p-1">Sign Up</h3>
 								&nbsp;
-								<p className="forgot-password">
-									Already registered? <a href="/Log in">Log in</a>
+								<p id="already-registered">
+									Already registered? <Link to="/login">Log In</Link>
 								</p>
 								<form
 									onSubmit={handleSubmit}
@@ -74,98 +118,132 @@ function SignUp() {
 									noValidate
 								>
 									<div className="form-group">
-										<div className="d-flex flex-row py-2">
-											<input
-												className="form-control w-100 form-control-lg"
-												type="text"
-												name="firstName"
-												placeholder="First Name"
-												value={formData.firstName}
-												onChange={(e) => handleInputChange(e)}
-												maxLength="100"
-												required
-											/>
+										<label htmlFor="firstName"></label>
+										<input
+											id="firstName"
+											className={`form-control w-100 form-control-lg ${
+												!isValid && formData.firstName.trim() === ""
+													? "is-invalid"
+													: ""
+											}`}
+											type="text"
+											name="firstName"
+											placeholder="First Name"
+											value={formData.firstName}
+											onChange={(e) => handleInputChange(e)}
+											maxLength="100"
+											required
+										/>
+										<div className="invalid-feedback text-start">
+											Please enter your first name.
 										</div>
 									</div>
+
 									<div className="form-group">
-										<div className="d-flex flex-row py-2">
-											<input
-												className="form-control w-100 form-control-lg"
-												type="text"
-												name="lastName"
-												placeholder="Last Name"
-												value={formData.lastName}
-												onChange={(e) => handleInputChange(e)}
-												maxLength="100"
-												required
-											/>
+										<label htmlFor="lastName"></label>
+										<input
+											id="lastName"
+											className={`form-control w-100 form-control-lg ${
+												!isValid && formData.lastName.trim() === ""
+													? "is-invalid"
+													: ""
+											}`}
+											type="text"
+											name="lastName"
+											placeholder="Last Name"
+											value={formData.lastName}
+											onChange={(e) => handleInputChange(e)}
+											maxLength="100"
+											required
+										/>
+										<div className="invalid-feedback text-start">
+											Please enter your last name.
 										</div>
 									</div>
+
 									<div className="form-group">
+										<label htmlFor="department"></label>
 										<select
-											className="form-control w-100 form-control-lg"
+											id="department"
+											className={`form-control w-100 form-control-lg ${
+												!isValid && formData.department.trim() === ""
+													? "is-invalid"
+													: ""
+											}`}
 											name="department"
 											value={formData.department}
 											onChange={(e) => handleInputChange(e)}
 											required
 										>
 											<option value="" disabled>
-												Select Department
+												Department
 											</option>
-											<option value="sales">HR Team</option>
-											<option value="marketing">Front-end Developers</option>
-											<option value="it">UI/UX Team</option>
+											<option value="it">IT</option>
+											<option value="marketing">Marketing</option>
+											<option value="finance">Finance</option>
+											<option value="sales">Sales</option>
+											<option value="hr">HR</option>
+											<option value="customer service">Customer Service</option>
+											<option value="legal">Legal</option>
+											<option value="operations">Operations</option>
 										</select>
-									</div>
-									<div className="form-group">
-										<div className="d-flex flex-row py-2">
-											<input
-												className="form-control w-100 form-control-lg"
-												type="email"
-												name="email"
-												placeholder="Enter Email"
-												value={formData.email.toLowerCase()}
-												onChange={(e) => handleInputChange(e)}
-												maxLength="500"
-												required
-											/>
-											<div
-												id="validationEmail"
-												className="invalid-feedback text-start"
-											>
-												Please enter your email address.
-											</div>
+										<div className="invalid-feedback text-start">
+											Please select your department.
 										</div>
 									</div>
+
 									<div className="form-group">
-										<div className="d-flex flex-row py-2">
-											<input
-												className="form-control w-100 form-control-lg"
-												type="password"
-												name="password"
-												placeholder="Enter Password"
-												value={formData.password}
-												onChange={(e) => handleInputChange(e)}
-												maxLength="200"
-												required
-											/>
-											<div
-												id="validationPassword"
-												className="invalid-feedback text-start"
-											>
-												Please enter your password.
-											</div>
+										<label htmlFor="email"></label>
+										<input
+											id="email"
+											className={`form-control w-100 form-control-lg `}
+											type="email"
+											name="email"
+											placeholder="Your Email"
+											value={formData.email.toLowerCase()}
+											onChange={(e) => handleInputChange(e)}
+											maxLength="500"
+											required
+										/>
+										<div
+											id="validationEmail"
+											className="invalid-feedback text-start"
+										>
+											Please enter your email address.
 										</div>
 									</div>
-									<br />
+
+									<div className="form-group">
+										<label htmlFor="password"></label>
+										<input
+											id="password"
+											className={`form-control w-100 form-control-lg ${
+												!isValid && formData.password.trim() === ""
+													? "is-invalid"
+													: ""
+											}`}
+											type="password"
+											name="password"
+											placeholder="Password"
+											value={formData.password}
+											onChange={(e) => handleInputChange(e)}
+											maxLength="200"
+											required
+										/>
+										<div className="invalid-feedback text-start">
+											Please enter your password.
+										</div>
+									</div>
+
 									<div className="row d-flex justify-content-center p-3">
 										<button
 											type="submit"
 											className="btn sign_in_btn btn-lg w-100"
 										>
-											Sign In
+											Sign Up
 										</button>
 									</div>
+
 									{signUpError && (
 										<h6 className="signup-error">
 											SignUp error: Please enter the correct information.
@@ -174,8 +252,9 @@ function SignUp() {
 								</form>
 								<div className="text-center">
 									<div>
-										By clicking Continue with or Log in, you agree to
-										<br /> Deskeando
+										By clicking Continue with or Sign up, you agree to
+										<br />
+										<b> Deskeando</b>
 										<span className="font-weight-bold d-flex justify-content-center">
 											Terms of Service and Privacy Policy
 										</span>
