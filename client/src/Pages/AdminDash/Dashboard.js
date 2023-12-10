ac0083558e6b75c5daf00d4f0aa3e49872ec0d27
Chidimma <chidimmamofodum@gmail.com>
2023-12-10 16:09:34 -0500

    route to forbidden page if user is not logged in

--- client/src/Pages/AdminDash/Dashboard.js
+++ client/src/Pages/AdminDash/Dashboard.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Forbidden from "../Error/Forbidden";
 
 const Dashboard = () => {
-    let name = useLocation().state.key
-    return (
-      <div>
-          <h2>{`Hello ${name} welcome to the admin dashboard`}</h2>
-    </div>
-  )
-}
+	let name = useLocation().state?.key;
+	return (
+		<div>
+			{name ? (
+				<h2>{`Hello ${name} welcome to the admin dashboard`}</h2>
+			) : (
+				<Forbidden />
+			)}
+		</div>
+	);
+};
 
-export default Dashboard
+export default Dashboard;
--- client/src/Pages/EmployeeDash/EmployeeDsh.js
+++ client/src/Pages/EmployeeDash/EmployeeDsh.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
-
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Forbidden from "../Error/Forbidden";
 
 const EmployeeDsh = () => {
-    let name = useLocation().state.key
-  return (
-    <div>
-          <h1>{`Hello ${name} welcome to the employee dashboard`}</h1>
-    </div>
-  )
-}
+	let name = useLocation().state?.key || null;
+	return (
+		<div>
+			{name ? (
+				<h1>{`Hello ${name} welcome to the employee dashboard`}</h1>
+			) : (
+				<Forbidden />
+			)}
+		</div>
+	);
+};
 
-export default EmployeeDsh
+export default EmployeeDsh;
