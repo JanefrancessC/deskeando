362d5fa22abc0d19b817c2a18f076b8ffc085232
Chidimma <chidimmamofodum@gmail.com>
2023-12-13 14:00:45 -0500

    changing variable names

--- client/src/Pages/AdminDash/Dashboard.js
+++ client/src/Pages/AdminDash/Dashboard.js
@@ -3,11 +3,11 @@ import { useLocation } from "react-router-dom";
 import Forbidden from "../Error/Forbidden";
 
 const Dashboard = () => {
-	let name = useLocation().state?.key;
+	let userName = useLocation().state?.key;
 	return (
 		<div>
-			{name ? (
-				<h2>{`Hello ${name} welcome to the admin dashboard`}</h2>
+			{userName ? (
+				<h2>{`Hello ${userName} welcome to the admin dashboard`}</h2>
 			) : (
 				<Forbidden />
 			)}
--- client/src/Pages/EmployeeDash/EmployeeDsh.js
+++ client/src/Pages/EmployeeDash/EmployeeDsh.js
@@ -3,11 +3,11 @@ import { useLocation } from "react-router-dom";
 import Forbidden from "../Error/Forbidden";
 
 const EmployeeDsh = () => {
-	let name = useLocation().state?.key || null;
+	let userName = useLocation().state?.key || null;
 	return (
 		<div>
-			{name ? (
-				<h1>{`Hello ${name} welcome to the employee dashboard`}</h1>
+			{userName ? (
+				<h1>{`Hello ${userName} welcome to the employee dashboard`}</h1>
 			) : (
 				<Forbidden />
 			)}
