e701cf1e08420135a7a2c5c748c12b1b0dc387e4
JanefrancessC <janefrancesschioma@gmail.com>
2023-12-13 04:59:06 +0000

    view user bookings

--- server/controllers/userBookings.js
+++ server/controllers/userBookings.js
@@ -1,16 +1,21 @@
-import db from "../db.js"
+import db from "../db.js";
 
-const userBookings = async (req, res) => {
-  try {
-    const id = req.params.user_id
+export const userBookings = async (req, res) => {
+	try {
+		const user = req.user;
+		const bookingResult = await db.query(
+			`SELECT * FROM bookings WHERE user_id = $1`,
+			[user.user_id]
+		);
+		const bookings = bookingResult.rows;
 
-    const bookings = await db.query(`SELECT * FROM bookings WHERE user_id = $1`, [id])
-
-    res.json(bookings)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: "Server error" })
-  }
-}
-
-export default userBookings
+		const bookingDetails = bookings.map((booking) => ({
+			"Booking ID": booking.booking_id,
+			"Desk ID": booking.desk_id,
+		}));
+		res.json(bookingDetails);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Server error" });
+	}
+};
