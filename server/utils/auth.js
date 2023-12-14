b6d781158fe9015be06f615bc94a3b80d6d1c5f2
JanefrancessC <janefrancesschioma@gmail.com>
2023-12-14 22:22:18 +0000

    generate token for a user

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,11 +5,11 @@ const authenticateToken = (req, res, next) => {
 	try {
 		const authHeader = req.headers["authorization"];
 		const token = authHeader && authHeader.split(" ")[1];
-		
+
 		if (token === null) {
 			return res.status(401).json({ error: "Invalid token" });
 		}
-		
+
 		jwt.verify(token, process.env.TOKEN_SECRET, (error, user) => {
 			if (error) return res.status(403).json({ error: error.message });
 			// set the request to the user input from jwtToken
@@ -19,9 +19,7 @@ const authenticateToken = (req, res, next) => {
 	} catch (error) {
 		console.error(error.message);
 		res.status(403).json({ error: "Access denied" });
-
 	}
 };
-
 
 export default authenticateToken;
--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -1,9 +1,8 @@
 import jwt from "jsonwebtoken";
 
-export const jwtToken = (user_id) => {
-    const payload = {
-        user: user_id,
-    }
+// generate a web token for user input details
+export const jwtToken = ({ user_id, email, first_name }) => {
+	const user = { user_id, email, first_name }
 
-    return jwt.sign(payload, process.env.TOKEN_SECRET, {expiresIn: "1800s"})
-}
+	return jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: "1800s" });
+};
