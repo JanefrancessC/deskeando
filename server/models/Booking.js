896aac1726ec54d7eadd899e57c5e6209ca3c36f
Chidimma <chidimmamofodum@gmail.com>
2023-12-14 14:19:52 -0500

    updated booking class

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,8 +1,10 @@
 export class Booking {
-    constructor({userId, deskId, createdAt}) {
+    constructor({userId, deskId, createdAt, reservationDate, updatedAt}) {
         this.userId = userId
         this.deskId = deskId
         this.createdAt = createdAt
+        this.reservationDate = reservationDate
+        this.updatedAt = updatedAt
     }
 }
 
