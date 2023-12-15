e588d759e34e2f3da9e295956a3513e45b4e37db
JanefrancessC <janefrancesschioma@gmail.com>
2023-12-15 21:03:24 +0000

    update user bookings

--- server/controllers/updateBooking.js
+++ server/controllers/updateBooking.js
@@ -1,27 +1,35 @@
 import db from "../db.js";
+import { viewBookings } from "./viewBookings.js";
 
 // update booking by id
 export const updateBooking = async (req, res) => {
-    try {
-        const user = req.user
-        console.log("user details: ", user);
-        const bookID = await db.query(`SELECT b.*, u.* FROM bookings b JOIN users u ON b.user_id = u.user_id WHERE u.user_id = $1`, [user.user_id])
-        console.log("bookID: ", bookID.rows[0]);
-        const bookingId = bookID.rows[0].booking_id;
-        // const { id: bookingId } = parseInt(req.params);
-        const { deskId, date } = req.body
-console.log("booking ID is: ", bookingId);
-        const prevBooking = await db.query(`SELECT * FROM bookings WHERE booking_id = $1 AND user_id = $2`, [bookingId, user.user_id])
-        
-        if (prevBooking.rows.length === 0) {
-            return res.status(404).json({ error: "No booking found" })
+	try {
+		const user = req.user;
+		const { desk, date } = req.body;
+		
+		// Get user booking details
+		const bookID = await db.query(
+			`SELECT b.*, u.* FROM bookings b JOIN users u ON b.user_id = u.user_id WHERE u.user_id = $1`,
+			[user.user_id]
+		);
+
+		if (bookID.rows.length === 0) {
+			return res.status(404).json({ error: "No booking found" });
+		}
+
+        // check if desk exists
+        const validDesk = await db.query(`SELECT desk_id FROM desks WHERE desk_name = $1`, [desk])
+        if (validDesk.rows.length === 0) {
+            return res.status(404).json({ error: "Not a valid desk" });
         }
 
-        const newBooking = await db.query(`UPDATE bookings SET desk_id = $1 WHERE booking_id = $2 RETURNING *`, [bookID.rows[0].desk_id, bookingId])
-        console.log(newBooking.rows[0]);
-        res.status(200).json({ message: "Booking updated" })
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error.message })
-    }
-}
+		const newBooking = await db.query(
+			`UPDATE bookings SET desk_id = $1, reservation_date = $2 WHERE booking_id = $3 RETURNING *`,
+			[validDesk.rows[0].desk_id, date, bookID.rows[0].booking_id]
+		);
+		res.status(200).json({ message: "Booking updated", updatedBooking: newBooking.rows[0] });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: error.message });
+	}
+};
