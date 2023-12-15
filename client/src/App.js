91bd35fe4c6fc9b98b5b25e9b8f5fb4a439aceb6
kavitappatil <patil.poem@gmail.com>
2023-12-15 17:01:37 +0000

    navbar, footer added

--- client/src/App.js
+++ client/src/App.js
@@ -7,26 +7,35 @@ import EmployeeDsh from "./Pages/EmployeeDash/EmployeeDsh.js";
 import SignIn from "./Pages/SignIn/SignIn.js";
 import Signup from "./Pages/Signup/Signup";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import NotFound from "./Pages/Error/NotFound.js";
 =======
 import NavBar from "./HomePage/Navbar/NavBar.js";
 import Hero from "./Pages/HomePage/Hero.js";
 >>>>>>> image and logo added
+=======
+import NavBar from "./NavBar/NavBar.js";
+import Footer from "./Footer/Footer.js";
+>>>>>>> navbar, footer added
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/signin" element={<SignIn />} />
 		<Route path="/signup" element={<Signup />} />
-		<Route path="/navbar" element={<NavBar />} />
 		<Route path="/about/this/site" element={<About />} />
 		<Route path="/admin" element={<Dashboard />} />
 		<Route path="/employee" element={<EmployeeDsh />} />
+<<<<<<< HEAD
 <<<<<<< HEAD
 		<Route path="*" element={<NotFound />} />
 =======
 		<Route path="/hero" element={<Hero />} />
 >>>>>>> image and logo added
+=======
+		<Route path="/navbar" element={<NavBar />} />
+		<Route path="/footer" element={<Footer />} />
+>>>>>>> navbar, footer added
 	</Routes>
 );
 
