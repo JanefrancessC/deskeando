72cf4dff6f500c2120f2796deaf5df875179a245
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2024-01-05 19:20:48 +0000

    validation e-mail

--- client/src/Pages/Signup/SignupForm.js
+++ client/src/Pages/Signup/SignupForm.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./signupForm.css";
 import { Link, Navigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import thelogo from "../../images/logo-02.png";
+import thefirstlogo from "../../images/logo-icon-01.png";
 
 function SignUp() {
 	const navigate = useNavigate();
@@ -18,6 +20,7 @@ function SignUp() {
 		password: "",
 	});
 	const [signUpError, setSignUpError] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
 
 	const handleInputChange = (event) => {
 		const { name, value } = event.target;
@@ -50,23 +53,64 @@ function SignUp() {
 	};
 
 	const trySignUp = async (url, data = {}) => {
-		const options = {
+		// User does not exist, proceed with signup
+		const signUpOptions = {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
 			},
 			body: JSON.stringify(data),
 		};
-		const response = await fetch(url, options);
+
+		const response = await fetch(url, signUpOptions);
+
 		if (response.status === 200) {
+			console.log(response);
 			resetForm();
 			navigate("/signin", { state: { key: "value" } });
 		} else {
+			let error = await response.json();
+			console.log(error);
+			setErrorMessage(error.error);
+			// Handle signup error
+			setSignUpError(true);
 		}
 	};
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
+
+		// Validate first name
+		if (formData.firstName.trim().length < 3) {
+			setValidationErrors((prevErrors) => ({
+				...prevErrors,
+				firstName: "First name must have at least 3 characters.",
+			}));
+			setIsValid(false);
+			setSignUpError(true);
+			return;
+		} else {
+			setValidationErrors((prevErrors) => ({
+				...prevErrors,
+				firstName: "",
+			}));
+		}
+
+		// Validate last name
+		if (formData.lastName.trim().length < 3) {
+			setValidationErrors((prevErrors) => ({
+				...prevErrors,
+				lastName: "Last name must have at least 3 characters.",
+			}));
+			setIsValid(false);
+			setSignUpError(true);
+			return;
+		} else {
+			setValidationErrors((prevErrors) => ({
+				...prevErrors,
+				lastName: "",
+			}));
+		}
 
 		if (!validateEmail(formData.email)) {
 			setValidationErrors((prevErrors) => ({
@@ -113,8 +157,6 @@ function SignUp() {
 		});
 	};
 
-	useEffect(() => {}, []);
-
 	return (
 		<section className="vh-100">
 			<div className="container py-5 h-75">
@@ -122,12 +164,22 @@ function SignUp() {
 					<div className="col-12 col-md-8 col-lg-6 col-xl-5">
 						<div className="card shadow-2-strong">
 							<div className="card-body p-5 text-center">
-								<p
-									id="title"
-									className="text-center h1 sign_up-heading fw-bold p-2"
-								>
-									Deskeando
-								</p>
+								<article className="bothLogos">
+									<img
+										src={thefirstlogo}
+										className="theFirstlogo"
+										alt="Logo"
+										// height="90"
+										// width="280"
+									/>
+									<img
+										src={thelogo}
+										className="theSecondlogo"
+										alt="Logo"
+										// height="100"
+										// width="280"
+									/>
+								</article>
 								<h3 className="form-group d-flex flex-row p-1">Sign Up</h3>
 								&nbsp;
 								<p id="already-registered">
@@ -268,8 +320,8 @@ function SignUp() {
 									</div>
 
 									{signUpError && (
-										<h6 className="signup-error">
-											SignUp error: Please enter the correct information.
+										<h6 className="signup-error" role="alert">
+											{errorMessage}
 										</h6>
 									)}
 								</form>
