2cbd9cb35b606bbdd46d6240d32376af3bd55ccf
Johng117 <83654331+Johng117@users.noreply.github.com>
2024-01-12 15:21:04 +0000

    Added Floorplan Component folder and floorplan components

---
+++ client/src/Components/FloorPlan/Furniture/Sofa.js
@@ -0,0 +1,100 @@
+import "./Sofa.css"
+const Sofa = ({xPoint,yPoint}) => {
+	return (
+		<svg
+			id="Layer_1"
+			data-name="Layer 1"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 273.84 288.29"
+			height="150"
+			width="165"
+			x={xPoint}
+			y={yPoint}
+		>
+			<defs>
+				{/* <style>.cls-1-sofa{fill:none;stroke:#1d1d1b;stroke-miterlimit:10;}</style> */}
+			</defs>
+			<rect
+				class="cls-1-sofa"
+				x="214.03"
+				y="-31.91"
+				width="16.08"
+				height="91.23"
+				rx="4.21"
+				transform="translate(235.78 -208.36) rotate(90)"
+			/>
+			<rect
+				class="cls-1-sofa"
+				x="214.03"
+				y="122.92"
+				width="16.08"
+				height="91.23"
+				rx="4.21"
+				transform="translate(390.61 -53.53) rotate(90)"
+			/>
+			<rect
+				class="cls-1-sofa"
+				x="251.6"
+				y="21.75"
+				width="16.08"
+				height="138.75"
+				rx="4.21"
+			/>
+			<path
+				class="cls-1-sofa"
+				d="M189.18,21.75H247.4A4.21,4.21,0,0,1,251.6,26v61a4.21,4.21,0,0,1-4.21,4.21H189.18a12.72,12.72,0,0,1-12.72-12.72V34.47A12.72,12.72,0,0,1,189.18,21.75Z"
+			/>
+			<path
+				class="cls-1-sofa"
+				d="M187.29,91.13h60.1a4.21,4.21,0,0,1,4.21,4.21v61a4.21,4.21,0,0,1-4.21,4.21h-60.1a10.84,10.84,0,0,1-10.84-10.84V102A10.84,10.84,0,0,1,187.29,91.13Z"
+			/>
+			<rect
+				class="cls-1-sofa"
+				x="159.17"
+				y="186.54"
+				width="16.08"
+				height="91.23"
+				rx="4.21"
+				transform="translate(334.43 464.31) rotate(180)"
+			/>
+			<rect
+				class="cls-1-sofa"
+				x="4.34"
+				y="186.54"
+				width="16.08"
+				height="91.23"
+				rx="4.21"
+				transform="translate(24.77 464.31) rotate(180)"
+			/>
+			<rect
+				class="cls-1-sofa"
+				x="81.76"
+				y="200.35"
+				width="16.08"
+				height="138.75"
+				rx="4.21"
+				transform="translate(359.53 179.93) rotate(90)"
+			/>
+			<path
+				class="cls-1-sofa"
+				d="M99.63,189.42h58.22a4.21,4.21,0,0,1,4.21,4.21v61a4.21,4.21,0,0,1-4.21,4.21H99.63a12.72,12.72,0,0,1-12.72-12.72V202.14A12.72,12.72,0,0,1,99.63,189.42Z"
+				transform="translate(348.59 99.63) rotate(90)"
+			/>
+			<path
+				class="cls-1-sofa"
+				d="M28.36,189.42h60.1a4.21,4.21,0,0,1,4.21,4.21v61a4.21,4.21,0,0,1-4.21,4.21H28.36A10.84,10.84,0,0,1,17.53,248V200.26A10.84,10.84,0,0,1,28.36,189.42Z"
+				transform="translate(279.21 169.01) rotate(90)"
+			/>
+			<rect
+				class="cls-1-sofa"
+				x="59.93"
+				y="31.01"
+				width="52.9"
+				height="98.76"
+				rx="9.86"
+			/>
+		</svg>
+	);
+};
+
+export default Sofa;
---
+++ client/src/Components/FloorPlan/SingleDesks/SmallLaptopDesk.js
@@ -0,0 +1,285 @@
+import "./SmallLaptopDesk.css";
+
+const SmallLaptopDesk = () => {
+	return (
+		<svg
+			id="Layer 1"
+			data-name="Layer 1"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlnsXlink="http://www.w3.org/1999/xlink"
+			viewBox="0 0 141.78 111.9"
+			height="95"
+			width="90"
+		>
+			<defs>
+				{/* <style>.cls-1-sm-laptop{fill:#676766;stroke-width:0.25px;}.cls-1-sm-laptop,.cls-2-sm-laptop{stroke:#1d1d1b;stroke-miterlimit:10;}.cls-2-sm-laptop{fill:#999;}.cls-3-sm-laptop{fill:#010101;}.cls-4-sm-laptop{fill:#e9e8e8;}.cls-5-sm-laptop{fill:#fff;}.cls-6-sm-laptop{fill:#d9d9d9;}</style> */}
+			</defs>
+			<g transform="scale(1 -1) translate(0,-100)">
+				<image
+					width="130"
+					height="141"
+					transform="translate(53.81 11.05) scale(0.24)"
+					xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIIAAACNCAYAAAB7VaIoAAAACXBIWXMAAC4jAAAuIwF4pT92AAACRklEQVR4Xu3XS1LbUBBA0WuyjGR5ZlV4r84grRQfg2UgFds6Z+SiXmnS97XE7ng89h2eDofveRAXe9zvd+fOnLP7SgiGf30+G8XFIRj+7bgkitUhCOB2rQni4dyBEsGtWzO/DzfCmgdwOz7aDO+GIIL7dSqIk68GEWzPqm8E7supi/4mBNtgG17P+UUIItgur4YNe37x/4ZgG2zTMncbgWpCsA2wEaiEwBACPR0ORyFQ2QiMHz9//fIfAzYCfwiBSggMIVAJgSEEKiEwhEAlBIYQqITAEAKVEBhCoBICQwhUQmAIgUoIDCFQCYEhBCohMIRAJQSGEKiEwBAClRAYQqASAkMIVEJgCIFKCAwhUAmBIQQqITCEQCUEhhCohMAQApUQGEKgEgJDCFRCYAiBSggMIVAJgSEEKiEwhEAlBIYQqITAEAKVEBhCoBICQwhUQmAIgUoIDCFQCYEhBCohMIRAJQSGEKiEwBAClRAYQqASAkMIVEJgCIFKCAwhUAmBIQQqITCEQCUEhhCohMAQApUQGEKgEgLj4XG/3507xP2zEaiEwBAClRAYQqDH/X73sPw4d5j7ZiNQCWHzlrfBw+s/sE02woY9v/wvQrAVtstG2KjXl/5NCLbCNp3cCGK4b6fm++6rQQz36b25+kbYkI8u94ch2ArbsTsej+fO9HQ4nD/E1VpzoVeFsBDE7VkTQV34jbD2oVyHS+Z10UZY2AzX7ZIAFp8K4TlRXI/PBLD4cggLQfw/Xwlg8W0hvCaMf+M7hn7Kb5pWjCSc4jhYAAAAAElFTkSuQmCC"
+				/>
+
+				<image
+					width="115"
+					height="21"
+					transform="translate(56.21 3.85) scale(0.24)"
+					xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHMAAAAVCAYAAAB17tGhAAAACXBIWXMAAC4jAAAuIwF4pT92AAAAq0lEQVRoQ+3ZMQ6DMAxA0ThnDZdquGs6IVGKZAQu2O7/U4QyRHpKBiNjjOKxPs8+D1ZKmVoTbc8Tyd2YnpGsuxvdFPOfoKyyBL+MCaBdV2FPYQL4+87AHsYE8LmOwqqYIPpJQ93FBNB/e7AfmCDGa41alwWQMVu71e0HitfiJ6/egUxSLZQmMBMFZqLATBSYiQIzUVWb91GMptbkazbLACFO24vIX5NAaa/oG6EMXXFy9RU+AAAAAElFTkSuQmCC"
+				/>
+
+				<path
+					className="cls-1-sm-laptop"
+					d="M88.91,15.9c1.39,0,2.55,1.45,2.59,3.31l.39,17.69c0,1.86-1.06,3.39-2.45,3.42s-2.56-1.45-2.6-3.31l-.38-17.69C86.42,17.46,87.52,15.93,88.91,15.9Z"
+				/>
+				<path
+					className="cls-1-sm-laptop"
+					d="M49.28,15.87c1.39,0,2.55,1.45,2.59,3.31l.39,17.69c0,1.86-1.06,3.39-2.45,3.42s-2.56-1.45-2.6-3.31l-.38-17.69C46.79,17.43,47.89,15.9,49.28,15.87Z"
+				/>
+
+				<rect
+					className="cls-2-sm-laptop"
+					x="3.28"
+					y="48.39"
+					width="134.07"
+					height="60.19"
+					rx="5.9"
+				/>
+				<polygon
+					className="cls-3-sm-laptop"
+					points="40.78 94.51 100 94.5 92.14 88.55 48.64 88.55 40.78 94.51"
+				/>
+
+				<rect
+					className="cls-4-sm-laptop"
+					x="40.78"
+					y="94.17"
+					width="59.21"
+					height="3.74"
+				/>
+				<polygon
+					className="cls-4-sm-laptop"
+					points="65.66 88.55 75.53 88.55 75.53 86.95 65.66 86.95 65.66 88.55"
+				/>
+
+				<path
+					className="cls-5-sm-laptop"
+					d="M53.39,82h34a3.41,3.41,0,0,0,3.41-3.41v-5a3.4,3.4,0,0,0-3.41-3.41h-34A3.4,3.4,0,0,0,50,73.54V78.6A3.41,3.41,0,0,0,53.39,82Z"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="85.98"
+					y="71.38"
+					width="2.05"
+					height="1.63"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="82.65"
+					y="71.38"
+					width="2.05"
+					height="1.63"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="79.33"
+					y="71.38"
+					width="2.05"
+					height="1.63"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="76.01"
+					y="71.38"
+					width="2.05"
+					height="1.63"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="72.68"
+					y="71.38"
+					width="2.05"
+					height="1.63"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="69.36"
+					y="71.38"
+					width="2.05"
+					height="1.63"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="66.04"
+					y="71.38"
+					width="2.05"
+					height="1.63"
+					transform="translate(-0.14 0.13) rotate(-0.11)"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="62.72"
+					y="71.38"
+					width="2.05"
+					height="1.63"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="59.4"
+					y="71.38"
+					width="2.04"
+					height="1.62"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="56.07"
+					y="71.38"
+					width="2.05"
+					height="1.63"
+					transform="translate(-0.08 0.07) rotate(-0.07)"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="52.96"
+					y="71.17"
+					width="1.63"
+					height="2.05"
+					transform="matrix(0, -1, 1, 0, -18.48, 125.88)"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="85.98"
+					y="74.45"
+					width="2.05"
+					height="1.62"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="82.65"
+					y="74.45"
+					width="2.05"
+					height="1.62"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="79.33"
+					y="74.45"
+					width="2.05"
+					height="1.62"
+				/>
+				<polygon
+					className="cls-6-sm-laptop"
+					points="76.01 76.07 78.05 76.08 78.06 74.45 76.01 74.45 76.01 76.07"
+				/>
+				<polygon
+					className="cls-6-sm-laptop"
+					points="72.68 76.08 74.73 76.08 74.74 74.45 72.69 74.45 72.68 76.08"
+				/>
+				<polygon
+					className="cls-6-sm-laptop"
+					points="69.36 76.08 71.41 76.07 71.42 74.45 69.37 74.45 69.36 76.08"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="66.25"
+					y="74.24"
+					width="1.63"
+					height="2.05"
+					transform="translate(-8.37 142.13) rotate(-89.85)"
+				/>
+				<polygon
+					className="cls-6-sm-laptop"
+					points="62.72 76.08 64.76 76.07 64.77 74.45 62.72 74.45 62.72 76.08"
+				/>
+				<polygon
+					className="cls-6-sm-laptop"
+					points="59.39 76.08 61.44 76.07 61.45 74.45 59.4 74.45 59.39 76.08"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="56.07"
+					y="74.45"
+					width="2.05"
+					height="1.62"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="52.75"
+					y="74.45"
+					width="2.04"
+					height="1.63"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="86.19"
+					y="77.31"
+					width="1.62"
+					height="2.05"
+					transform="translate(8.5 165.19) rotate(-89.89)"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="82.66"
+					y="77.52"
+					width="2.05"
+					height="1.62"
+				/>
+				<polygon
+					className="cls-6-sm-laptop"
+					points="79.33 79.15 81.38 79.15 81.38 77.53 79.33 77.53 79.33 79.15"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="76.01"
+					y="77.52"
+					width="2.05"
+					height="1.62"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="72.69"
+					y="77.53"
+					width="2.05"
+					height="1.62"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="69.36"
+					y="77.53"
+					width="2.05"
+					height="1.62"
+					transform="translate(-0.1 0.09) rotate(-0.07)"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="66.04"
+					y="77.52"
+					width="2.05"
+					height="1.62"
+					transform="translate(-0.19 0.17) rotate(-0.14)"
+				/>
+				<rect
+					className="cls-6-sm-laptop"
+					x="62.72"
+					y="77.53"
+					width="2.05"
+					height="1.62"
+				/>
+				<polygon
+					className="cls-6-sm-laptop"
+					points="59.4 79.14 61.45 79.15 61.44 77.53 59.39 77.53 59.4 79.14"
+				/>
+				<polygon
+					className="cls-6-sm-laptop"
+					points="56.07 79.15 58.12 79.15 58.12 77.53 56.07 77.53 56.07 79.15"
+				/>
+				<polygon
+					className="cls-6-sm-laptop"
+					points="52.75 79.15 54.8 79.15 54.8 77.52 52.75 77.53 52.75 79.15"
+				/>
+			</g>
+		</svg>
+	);
+};
+export default SmallLaptopDesk;
