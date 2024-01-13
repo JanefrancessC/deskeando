60152bc71b576adccfdf36a28f78e10886538579
Johng117 <83654331+Johng117@users.noreply.github.com>
2024-01-13 11:29:13 +0000

    Updated components and added styling
    Updated and added components. Changed tooltip library.
    Updated styles.

--- client/src/Components/FloorPlan/Furniture/Sofa.js
+++ client/src/Components/FloorPlan/Furniture/Sofa.js
@@ -6,13 +6,13 @@ const Sofa = ({xPoint,yPoint}) => {
 			data-name="Layer 1"
 			xmlns="http://www.w3.org/2000/svg"
 			viewBox="0 0 273.84 288.29"
-			height="150"
-			width="165"
+			height="180"
+			width="195"
 			x={xPoint}
 			y={yPoint}
 		>
 			<rect
-				class="cls-1-sofa"
+				className="cls-1-sofa"
 				x="214.03"
 				y="-31.91"
 				width="16.08"
@@ -21,7 +21,7 @@ const Sofa = ({xPoint,yPoint}) => {
 				transform="translate(235.78 -208.36) rotate(90)"
 			/>
 			<rect
-				class="cls-1-sofa"
+				className="cls-1-sofa"
 				x="214.03"
 				y="122.92"
 				width="16.08"
@@ -30,7 +30,7 @@ const Sofa = ({xPoint,yPoint}) => {
 				transform="translate(390.61 -53.53) rotate(90)"
 			/>
 			<rect
-				class="cls-1-sofa"
+				className="cls-1-sofa"
 				x="251.6"
 				y="21.75"
 				width="16.08"
@@ -38,15 +38,15 @@ const Sofa = ({xPoint,yPoint}) => {
 				rx="4.21"
 			/>
 			<path
-				class="cls-1-sofa"
+				className="cls-1-sofa"
 				d="M189.18,21.75H247.4A4.21,4.21,0,0,1,251.6,26v61a4.21,4.21,0,0,1-4.21,4.21H189.18a12.72,12.72,0,0,1-12.72-12.72V34.47A12.72,12.72,0,0,1,189.18,21.75Z"
 			/>
 			<path
-				class="cls-1-sofa"
+				className="cls-1-sofa"
 				d="M187.29,91.13h60.1a4.21,4.21,0,0,1,4.21,4.21v61a4.21,4.21,0,0,1-4.21,4.21h-60.1a10.84,10.84,0,0,1-10.84-10.84V102A10.84,10.84,0,0,1,187.29,91.13Z"
 			/>
 			<rect
-				class="cls-1-sofa"
+				className="cls-1-sofa"
 				x="159.17"
 				y="186.54"
 				width="16.08"
@@ -55,7 +55,7 @@ const Sofa = ({xPoint,yPoint}) => {
 				transform="translate(334.43 464.31) rotate(180)"
 			/>
 			<rect
-				class="cls-1-sofa"
+				className="cls-1-sofa"
 				x="4.34"
 				y="186.54"
 				width="16.08"
@@ -64,7 +64,7 @@ const Sofa = ({xPoint,yPoint}) => {
 				transform="translate(24.77 464.31) rotate(180)"
 			/>
 			<rect
-				class="cls-1-sofa"
+				className="cls-1-sofa"
 				x="81.76"
 				y="200.35"
 				width="16.08"
@@ -73,17 +73,17 @@ const Sofa = ({xPoint,yPoint}) => {
 				transform="translate(359.53 179.93) rotate(90)"
 			/>
 			<path
-				class="cls-1-sofa"
+				className="cls-1-sofa"
 				d="M99.63,189.42h58.22a4.21,4.21,0,0,1,4.21,4.21v61a4.21,4.21,0,0,1-4.21,4.21H99.63a12.72,12.72,0,0,1-12.72-12.72V202.14A12.72,12.72,0,0,1,99.63,189.42Z"
 				transform="translate(348.59 99.63) rotate(90)"
 			/>
 			<path
-				class="cls-1-sofa"
+				className="cls-1-sofa"
 				d="M28.36,189.42h60.1a4.21,4.21,0,0,1,4.21,4.21v61a4.21,4.21,0,0,1-4.21,4.21H28.36A10.84,10.84,0,0,1,17.53,248V200.26A10.84,10.84,0,0,1,28.36,189.42Z"
 				transform="translate(279.21 169.01) rotate(90)"
 			/>
 			<rect
-				class="cls-1-sofa"
+				className="cls-1-sofa"
 				x="59.93"
 				y="31.01"
 				width="52.9"
