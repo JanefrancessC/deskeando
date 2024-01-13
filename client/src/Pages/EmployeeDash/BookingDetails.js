bc90bd860671b3af2c90b3ee2ccf59b8b9bccc66
Chidimma <chidimmamofodum@gmail.com>
2024-01-13 09:14:28 -0500

    floor plan displayed on icon click

--- client/src/Pages/EmployeeDash/BookingDetails.js
+++ client/src/Pages/EmployeeDash/BookingDetails.js
@@ -1,18 +1,23 @@
 import React from "react";
 import BookingForm from "./Form/BookingForm";
 import Table from "./Table/Table";
+import FloorPlan from "../../Components/FloorPlan/FloorPlan";
 import "./EmployeeDsh.css";
 
-const BookingDetails = ({ isSplitView }) => {
+const BookingDetails = ({ view }) => {
 	return (
 		<div class="w-100 d-flex justify-content-around">
-			{isSplitView ? (
+			{view.floorPlan ? (
+				<div className="w-75">
+					<FloorPlan />
+				</div>
+			) : view.listView.splitView ? (
 				<>
 					<BookingForm />
-					<Table isSplitView={isSplitView} />
+					<Table isSplitView={view.listView.splitView} />
 				</>
 			) : (
-				<Table isSplitView={isSplitView} />
+				<Table isSplitView={view.listView.splitView} />
 			)}
 		</div>
 	);
