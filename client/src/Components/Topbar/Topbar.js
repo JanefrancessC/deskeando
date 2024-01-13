411b008bf6f66bc3bc45d76bf6006f234a5635a6
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2024-01-13 20:25:33 +0000

    update

--- client/src/Components/Topbar/Topbar.js
+++ client/src/Components/Topbar/Topbar.js
@@ -3,9 +3,21 @@ import "./topbar.css";
 import iconuser from "./icon-user.svg";
 import bell from "./bell.svg";
 import { Link } from "react-router-dom";
+import searchIcon from "./searchIcon.svg";
+// import Home from "../SideBar/HomeIcon";
+// import Map from "..SideBar/FloorplanIcon";
 
-function Topbar({ userDetails, handleClick }) {
-	const [showDropdown, setShowDropdown] = useState(false)
+function Topbar(prop) {
+	const [startDate, setStartDate] = useState(null);
+	const [userName, setUserName] = useState("");
+	const [userRole, setUserRole] = useState("");
+	const [showDropdown, setShowDropdown] = useState(false);
+	const [navItems, setNavItems] = useState([
+		// { name: "Home", component: <Home /> },
+		{ name: "Floor Plan", component: <Map /> },
+		{ name: "Book Desk", icon: "bi-calendar-date" },
+		{ name: "Settings", icon: "bi-gear" },
+	]);
 
 	const dropdownRef = useRef(null);
 
@@ -19,18 +31,48 @@ function Topbar({ userDetails, handleClick }) {
 		}
 	};
 
-	return (
-		<nav id="navbar" className="navbar navbar-expand-lg  justify-content-end">
-			
+	const stopPropagation = (event) => {
+		event.stopPropagation();
+	};
 
-			<div className="end-of-page">
+	return (
+		<nav id="navbar" className="navbar navbar-expand-lg navbar-white bg-white">
+			<section className="first_section">
+				<form className=" form-inline ">
+					<div className="input-control-form form-group has-search">
+						<img className="searchIcon" src={searchIcon} alt="The Bell" />
+						<input
+							type="text"
+							className="input-control-form form-control"
+							placeholder=" Search here..."
+						/>
+					</div>
+				</form>
+			</section>
+			<button
+				className="navbar-toggler navbar-toggler-topbar "
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<section
+				id="navbarSupportedContent"
+				className="collapse navbar-collapse end-of-page"
+			>
+				{/* <div className="bell_user__role"> */}
 				<img className="bell" src={bell} alt="The Bell" />
-				{/* <img className="gear" src={gear} alt="The Gear" /> */}
+
 				<span className="user-info">
-					<strong className="user-name">{userDetails.userName}</strong> <br />
-					<span className="user-role">{userDetails.role}</span>
+					<strong className="user-name">{prop.userName}</strong> <br />
+					<span className="user-role">{prop.role}</span>
 				</span>
-				<div className="the-icon-user-background" ref={handleClickOutside}>
+				{/* </div> */}
+				<div className="the-icon-user-background" ref={dropdownRef}>
 					<div onClick={toggleDropdown}>
 						<img className="icon-user" src={iconuser} alt="The Blue Dot" />
 					</div>
@@ -45,8 +87,37 @@ function Topbar({ userDetails, handleClick }) {
 						</Link>
 					)}
 				</div>
-			</div>
+				<section className="second_logout">
+					<ul className="navbar-nav nav-bar-sd flex-column align-items-center mb-sm-auto mt-5 gap-2">
+						{navItems.map((item) => {
+							return (
+								<li
+									className="nav-item nav-item-sd rounded-start-pill  d-flex justify-content-center ps-3 pt-1"
+									onClick={() => console.log(item.name)}
+								>
+									<a
+										href=""
+										className="nav-link border-0 p-0 d-flex align-items-center gap-3"
+									>
+										{/* <i className={item.icon}></i>{" "} */}
+										<span className=" d-sm-inline">{item.name}</span>
+									</a>
+								</li>
+							);
+						})}
+					</ul>
+					<Link className="dropdown-button" to="/">
+						<button
+							className="dropdown-menu-buttons"
+							onClick={() => localStorage.clear()}
+						>
+							Sign out
+						</button>
+					</Link>
+				</section>
+			</section>
 		</nav>
 	);
 }
+
 export default Topbar;
