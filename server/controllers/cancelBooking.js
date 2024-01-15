97eb7835ed2a6dab03061cb18f5d74c3510b1505
JanefrancessC <janefrancesschioma@gmail.com>
2024-01-15 12:30:53 +0000

    delete bbooking updated

--- server/controllers/cancelBooking.js
+++ server/controllers/cancelBooking.js
@@ -8,28 +8,48 @@ export const cancelBooking = async (req, res) => {
 		if (!user) {
 			return res.status(401).json({ error: "Unauthorized action" });
 		}
+
 		if (typeof bookingIdToDelete !== "number" || isNaN(bookingIdToDelete)) {
-			return res.status(403).json({ error: "Not a valid Id" });
+			console.error(`Invalid Booking ID: ${bookingIdToDelete}`);
+			return res.status(400).json({ error: "Not a valid ID" });
 		}
-		const bookingDetails = await db.query(
-			`SELECT * FROM bookings WHERE booking_id = $1 AND user_id = $2`,
-			[bookingIdToDelete, user.user_id]
-		);
+		const bookingDetails = await db
+			.query(`SELECT * FROM bookings WHERE booking_id = $1 AND user_id = $2`, [
+				bookingIdToDelete,
+				user.user_id,
+			])
+			.catch((error) => {
+				console.error(error);
+				return res.status(500).json({ error: "Database error" });
+			});
+			console.log(`Booking Details: ${bookingDetails}`);
 
 		const booking = bookingDetails.rows[0];
+		console.log(`Bookings: ${booking}`);
 
 		if (!booking) {
 			res.status(404).json({ message: "Booking not found" });
 			return;
 		}
 
-		const deleteBooking = await db.query(
-			`DELETE FROM public.bookings WHERE booking_id = $1`,
-			[bookingIdToDelete]
-		);
+		const deleteBooking = await db
+			.query(`DELETE FROM public.bookings WHERE booking_id = $1`, [
+				bookingIdToDelete,
+			])
+			.catch((error) => {
+				console.error(error);
+				return res.status(500).json({ error: `Database Error` });
+			});
+
+		if (deleteBooking.rowCount === 0) {
+			return res.status(404).json({ message: "Booking not found2" });
+		}
+
 		res.status(200).json({ message: "Bookings deleted successfully" });
+
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: "Server error" });
+		return;
 	}
 };
