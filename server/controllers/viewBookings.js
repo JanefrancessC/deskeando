7b3afbf4bc2cd822fc6c7f2a75e10f62eaaf3c56
kavitappatil <patil.poem@gmail.com>
2024-01-19 21:58:29 +0000

    viewBookings uncommented part deleted

--- server/controllers/viewBookings.js
+++ server/controllers/viewBookings.js
@@ -5,17 +5,16 @@ export const viewBookings = async (req, res) => {
 	try {
 		const user = req.user;
 		let bookingDetails;
+
 		// Check if user is admin
 		const isAdmin = await userAdmin(user.user_id);
 
 		if (isAdmin) {
 			// Admin view
-			bookingDetails = await getAdminBookings()
-			
+			bookingDetails = await getAdminBookings();
 		} else {
 			// Employee view
-			bookingDetails = await getUserBookings(user.user_id)
-			
+			bookingDetails = await getUserBookings(user.user_id);
 		}
 
 		res.json(bookingDetails);
