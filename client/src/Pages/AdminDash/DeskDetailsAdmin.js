d654025fa9c5c400aca7c7d843af78100aeb7774
Johng117 <83654331+Johng117@users.noreply.github.com>
2024-01-23 16:30:53 +0000

    Update DeskDetailsAdmin.js

--- client/src/Pages/AdminDash/DeskDetailsAdmin.js
+++ client/src/Pages/AdminDash/DeskDetailsAdmin.js
@@ -7,56 +7,26 @@ import Table from "../EmployeeDash/Table/Table.js";
 import BookingDetailsAdmin from "./BookingDetailsAdmin";
 
 const DeskDetailsAdmin = ({ view, allDesks, setReload, setView }) => {
-	if (!view.floorPlan  && view.bookings===true)
-		return (
-			<div class="d-flex justify-content-around">
-				{/* <div className="floorPlan-wrapper"> */}
-					{/* <FloorPlan setView={setView} /> */}
-					<BookingDetailsAdmin />
-				{/* </div> */}
-			</div>
-		);
-	else if(!view.floorPlan && view.desks===true) {
-		if(view?.listView.splitView) {
-			return (
-				<div class="d-flex justify-content-around">
-				<DeskForm setReload={setReload} />
-	 			<DeskTable
-	 				isSplitView={view.listView.splitView}
-	 				allDesks={allDesks}
-	 			/>
-	 		</div>
-			)
-		}
-		// return (
-		// 	<div class="d-flex justify-content-around">
-		// 		{/* <div className="floorPlan-wrapper"> */}
-		// 			{/* <FloorPlan setView={setView} /> */}
-		// 			< />
-		// 		{/* </div> */}
-		// 	</div>
-		// );
-	}
-	// return (
-
-	// <div class="d-flex justify-content-around">
-	// 	{view.floorPlan ? (
-	// 		<div className="floorPlan-wrapper">
-	// 			<FloorPlan setView={setView}/>
-	// 		</div>
-	// 	) : view?.listView.splitView ? (
-	// 		<>
-	// 			<DeskForm setReload={setReload} />
-	// 			<DeskTable
-	// 				isSplitView={view.listView.splitView}
-	// 				allDesks={allDesks}
-	// 			/>
-	// 		</>
-	// 	) : (
-	// 		<BookingDetailsAdmin />
-	// 	)}
-	// </div>
-	// );
+	
+	return (
+		<div class="d-flex justify-content-around">
+			{view.floorPlan ? (
+				<div className="floorPlan-wrapper">
+					<FloorPlan setView={setView} />
+				</div>
+			) : view?.listView.splitView ? (
+				<>
+					<DeskForm setReload={setReload} />
+					<DeskTable
+						isSplitView={view.listView.splitView}
+						allDesks={allDesks}
+					/>
+				</>
+			) : (
+				<BookingDetailsAdmin />
+			)}
+		</div>
+	);
 };
 
 export default DeskDetailsAdmin;
