5f8ae5834f2f9cf389341b11c46b3087fc936573
JanefrancessC <janefrancesschioma@gmail.com>
2024-02-03 12:46:23 +0000

    cloud changes

--- server/app.js
+++ server/app.js
@@ -18,10 +18,10 @@ app.use(express.json());
 app.use(configuredHelmet());
 app.use(configuredMorgan());
 
-if (config.production) {
-  app.enable("trust proxy");
-  app.use(httpsOnly());
-}
+//if (config.production) {
+//  app.enable("trust proxy");
+//  app.use(httpsOnly());
+//}
 
 app.use(apiRoot, apiRouter);
 app.use("/health", (_, res) => res.sendStatus(200));
